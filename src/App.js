82b3ebebcf71a6d6f45caad0bde53aef79130e48
mohito <mypywelcome@gmail.com>
2024-10-06 17:00:20 +0400

    add_homepage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { darkTheme } from './theme/DarkTheme';
+import HomePage from './customer/pages/homePage/HomePage';
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={darkTheme}>
         <CssBaseline/>
-        <h1 className="text-3x1 font-bold underline">
-          Hello World!
-        </h1>
+        <HomePage/>
       </ThemeProvider>
      
     );
--- src/customer/pages/homePage/HomePage.jsx
+++ src/customer/pages/homePage/HomePage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import "./HomePage.css";
 
 const HomePage  = () => {
     return(
         <div>
-            <section className="-z-50 banner relative flex flex-col justify-center items-center">
-                <div className="w-[50vw] z-10 text-center">
-                    <p className="text-2xl lg:text-7xl font-bold z-10 py-5">Taco Cloud</p>
-                    <p className="z-10 text-gray-300 text-xl lg:test4xl">Taco Cloud</p>
-                </div>
-
-            </section>
+        <section className='-z-50 banner relative flex flex-col justify-center items-center'>
+            <div className='w-[50vw] z-10 text-center'>
+                <p className='text-2xl lg:text-7xl font-bold z-10 py-5'>Taco Cloud</p>
+                <p className='z-10 text-gray-300 text-xl lg:text-4xl'>
+                    Test the Convenience: Food, Fast And Delivered</p>
+            </div>
+        </section>
+        <div className='cover absolute top-0 left-0 right-0'></div>
+        <div className='fadout'>121212</div>
         </div>
     )
 }
