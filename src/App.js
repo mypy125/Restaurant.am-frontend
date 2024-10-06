21c9eb958814dc53ebfc739d20134470ce589895
mohito <mypywelcome@gmail.com>
2024-10-06 14:16:17 +0400

    add_homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './theme/DarkTheme';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        <h1 className="text-3x1 font-bold underline">
+          Hello World!
+        </h1>
+      </ThemeProvider>
+     
     );
   }
 }
---
+++ src/customer/pages/homePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage  = () => {
+    return(
+        <div>
+            <section className="-z-50 banner relative flex flex-col justify-center items-center">
+                <div className="w-[50vw] z-10 text-center">
+                    <p className="text-2xl lg:text-7xl font-bold z-10 py-5">Taco Cloud</p>
+                    <p className="z-10 text-gray-300 text-xl lg:test4xl">Taco Cloud</p>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+export default HomePage
